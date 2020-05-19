41671980883242b5bfd1bf8334995e6ed427bbd3
Deepak Singh <dsingh20800@gmail.com>
2020-05-19 14:24:42 +0100

    Change of pragma and unit used for ether calculaton

--- Faucet.sol
+++ Faucet.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.6.4;
+pragma solidity ^0.6.8;
 
 contract Faucet {
     function withdraw(uint256 withdraw_amount) public {
-        require(withdraw_amount <= 100000000000000000);
+        require(withdraw_amount <= 0.1 ether);
         msg.sender.transfer(withdraw_amount);
     }
 
