5470b18f1ddeba7d7e6119519ea0b66fbc952648
Deepak Singh <dsingh20800@gmail.com>
2020-05-22 17:27:15 +0100

    Initial code for deploying smart contract into Ganache test blockchain

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -6,3 +6,28 @@ const ganache = require('ganache-cli');
 
 /* --- Module for interfacing with ethereum node --- */
 const Web3 = require('web3');
+
+/* --- Get ABI and Bytecode from compiled Solidity contract --- */
+const { interface, bytecode } = require('../compile');
+
+/* --- Initiating web3 --- */
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+
+beforeEach(async () =>  {
+    /* Get a list of all accounts */
+    accounts = await web3.eth.getAccounts();
+
+    /* Deploy contract */
+    inbox = await new web3.eth.contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello'] })
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
