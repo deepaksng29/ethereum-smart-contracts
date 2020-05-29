e4bc5901cc3fa28c0c856c739420fa6b1e4e3033
Deepak Singh <dsingh20800@gmail.com>
2020-05-29 17:41:26 +0100

    Fixing whitespace issue

--- lottery/contracts/lottery.sol
+++ lottery/contracts/lottery.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.6.8;
 contract Lottery {
     address private manager;
     address[] private players;
-    
+
     constructor() public {
-        manager = msg.sender;    
+        manager = msg.sender;
     }
-    
+
     function GetManager() public view returns (address) {
         return manager;
     }
