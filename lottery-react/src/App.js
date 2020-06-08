aafbe672c2c0d245867866bd0a87713165278ba2
Deepak Singh <dsingh20800@gmail.com>
2020-06-08 19:52:17 +0100

    Front-end development

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -3,23 +3,69 @@ import logo from './logo.svg';
 import './App.css';
 import web3 from "./web3";
 import lotteryContract from "./lottery";
+import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { manager: "" };
-  }
+  /* ---- Initialisation of the state of the main component ---- */
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: ""
+  };
 
+  /* ---- Network requests to the Ethereum blockchain ---- */
   async componentDidMount() {
+    this.setState({ message: "Loading..." });
     const manager = await lotteryContract.methods.manager().call();
-    this.setState( { manager: manager } );
+    const players = await lotteryContract.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lotteryContract.options.address);
+    this.setState( { manager, players, balance, message: "" } );
   }
 
+  
+  /* ---- Event handling for entering lottery ---- */
+  onSubmit = async (event) => {
+    try{
+      this.setState({ message: "Entering you in the competition..." });
+      event.preventDefault();
+  
+      const accounts = await web3.eth.getAccounts();
+      
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, "ether")
+      });
+  
+      this.setState({ message: "You have been entered in the competition!" });
+      this.componentDidMount();
+    } catch (e) {
+      this.setState({ message: "Transaction failed." });
+    }
+  }
+
+  /* ---- Rendering the component using JSX ---- */
   render() {
     return (
       <div>
-        <h2>Lottery Contract</h2>
-        <p>This contract is managed by { this.state.manager }! </p>
+        <h2>Lottery Contract</h2>      
+        <p>
+          This contract is managed by { this.state.manager }! <br />
+          There are currently { this.state.players.length } players competing to win { web3.utils.fromWei(this.state.balance, "ether") } ETH!
+        </p>
+        <hr />
+        <form onSubmit = { this.onSubmit }>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ETH to enter: </label>
+            <input
+              onChange = { event => this.setState({ value: event.target.value }) } />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+        <p>{ this.state.message }</p>
       </div>
     )
   }
--- lottery-react/src/lottery.js
+++ lottery-react/src/lottery.js
@@ -2,57 +2,72 @@ import web3 from "./web3";
 
 /* ---- Contract address and ABI ---- */
 const contractAddress = "0x0DF055b0d5dcD91269EcCcc75470B56D13999742";
-const contractABI = [{
-    "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-}, {
-    "inputs": [],
-    "name": "enter",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-}, {
-    "inputs": [],
-    "name": "getPlayers",
-    "outputs": [{
-        "internalType": "address[]",
-        "name": "",
-        "type": "address[]"
-    }],
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "inputs": [],
-    "name": "manager",
-    "outputs": [{
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-    }],
-    "stateMutability": "view",
-    "type": "function"
-}, {
-    "inputs": [],
-    "name": "pickWinner",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-}, {
-    "inputs": [{
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-    }],
-    "name": "players",
-    "outputs": [{
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-    }],
-    "stateMutability": "view",
-    "type": "function"
-}]
+const contractABI = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    }, 
+    {
+        "inputs": [],
+        "name": "enter",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    }, 
+    {
+        "inputs": [],
+        "name": "getPlayers",
+        "outputs": [
+            {
+            "internalType": "address[]",
+            "name": "",
+            "type": "address[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }, 
+    {
+        "inputs": [],
+        "name": "manager",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }, 
+    {
+        "inputs": [],
+        "name": "pickWinner",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }, 
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "players",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
 
 /* ---- Local contract instance ---- */
 export default new web3.eth.Contract(contractABI, contractAddress);
