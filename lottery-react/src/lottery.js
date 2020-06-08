4ea21dfd21d6fd917718196280391db4d12e87ee
Deepak Singh <dsingh20800@gmail.com>
2020-06-08 18:28:56 +0100

    Fixed issue with instance of the contract

--- lottery-react/src/lottery.js
+++ lottery-react/src/lottery.js
@@ -1,68 +1,58 @@
 import web3 from "./web3";
 
 /* ---- Contract address and ABI ---- */
-const contractAddress = "0x5Ed98557EfA75163bcCce0F7Aa0074B76244CBA2";
-const contractABI = [ 
-    { 
-        inputs: [],
-        stateMutability: 'nonpayable',
-        type: 'constructor',
-        constant: undefined,
-        payable: undefined,
-        signature: 'constructor' 
-    },
-    { 
-        inputs: [],
-        name: 'enter',
-        outputs: [],
-        stateMutability: 'payable',
-        type: 'function',
-        constant: undefined,
-        payable: true,
-        signature: '0xe97dcb62' 
-    },
-    { 
-        inputs: [],
-        name: 'getPlayers',
-        outputs: [ [Object] ],
-        stateMutability: 'view',
-        type: 'function',
-        constant: true,
-        payable: undefined,
-        signature: '0x8b5b9ccc' 
-    },
-    { 
-        inputs: [],
-        name: 'manager',
-        outputs: [ [Object] ],
-        stateMutability: 'view',
-        type: 'function',
-        constant: true,
-        payable: undefined,
-        signature: '0x481c6a75' 
-    },
-    { 
-        inputs: [],
-        name: 'pickWinner',
-        outputs: [],
-        stateMutability: 'nonpayable',
-        type: 'function',
-        constant: undefined,
-        payable: undefined,
-        signature: '0x5d495aea' 
-    },
-    { 
-        inputs: [ [Object] ],
-        name: 'players',
-        outputs: [ [Object] ],
-        stateMutability: 'view',
-        type: 'function',
-        constant: true,
-        payable: undefined,
-        signature: '0xf71d96cb' 
-    }
-];
+const contractAddress = "0x0DF055b0d5dcD91269EcCcc75470B56D13999742";
+const contractABI = [{
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+}, {
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "inputs": [{
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+    }],
+    "name": "players",
+    "outputs": [{
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+    }],
+    "stateMutability": "view",
+    "type": "function"
+}]
 
 /* ---- Local contract instance ---- */
 export default new web3.eth.Contract(contractABI, contractAddress);
-
