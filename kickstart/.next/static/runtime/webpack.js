7b2f0b5e2e9c8144c96bb1a900908493b00b7f5c
Deepak Singh <dsingh20800@gmail.com>
2020-06-17 17:31:36 +0100

    Added compatibility without Web3.js injection from Metamask and Semantic UI

--- kickstart/.next/static/runtime/webpack.js
+++ kickstart/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7991c11e9edc7df7fd55";
+/******/ 	var hotCurrentHash = "37665267c798b6eed712";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
